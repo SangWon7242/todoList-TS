e8706819d992a218a517854f18ca5abdf244352b
SangWon7242 <gotkdqja@gmail.com>
2024-12-26 19:33:17 +0900

    숫자 기록, 초기화 기능 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,41 @@ import { useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(0);
+  const [recordedNumber, setRecordedNumber] = useState<number[]>([]);
 
   return (
-    <div>
-      <button onClick={() => setCount(count + 1)}>증가</button>
-      <button onClick={() => setCount(count - 1)}>감소</button>
-      <p>{count}</p>
-    </div>
+    <>
+      <div>
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count + 1)}>증가</button>
+        &nbsp;
+        <button onClick={() => setCount(count - 1)}>감소</button>
+        <br />
+        <button
+          onClick={() => {
+            if (count == 0) return;
+            setRecordedNumber([...recordedNumber, count]);
+          }}
+        >
+          기록
+        </button>
+        &nbsp;
+        <button
+          onClick={() => {
+            setCount(0);
+            setRecordedNumber([]);
+          }}
+        >
+          초기화
+        </button>
+      </div>
+      <div>
+        {recordedNumber.length == 0 ? (
+          <p>기록된 숫자가 없습니다.</p>
+        ) : (
+          <p>{JSON.stringify(recordedNumber)}</p>
+        )}
+      </div>
+    </>
   );
 }
